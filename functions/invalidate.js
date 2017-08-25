6fb090938bb453a50472dc57a42b7952d180528d
Ivo Abadjiev <ivo@Ivailos-MacBook-Pro.local>
2017-08-25 12:26:57 +0300

    looks like ReplayLambdaInvokePermission has to DependsOn: [ReplayLambdaFunction], AuthorizerLambdaInvokePermission has to DependsOn: [AuthorizerLambdaFunction] and InvalidateLambdaInvokePermission has to DependsOn: [InvalidateLambdaFunction]

--- functions/invalidate.js
+++ functions/invalidate.js
@@ -1,12 +1,22 @@
 'use strict';
 const request = require('request');
 module.exports.invalidate = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
+  const options = {
+    url: `${event.stageVariables.domain}/${event.stageVariables.stage}/stories`,
     headers: {
-      "Access-Control-Allow-Origin": "*" // Required for CORS support to work
-    },
-    body: JSON.stringify(event)
+      'Cache-Control': 'max-age=0'
+    }
   };
-  callback(null, response);
+  request(options, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+      },
+      body: JSON.stringify(event)
+    });  
+  });
 };
