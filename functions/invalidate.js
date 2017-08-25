a078ba646d606c8c1d1203edbf361906a28a6329
Ivo Abadjiev <ivo@Ivailos-MacBook-Pro.local>
2017-08-25 15:52:15 +0300

    cache works but cache invalidation still does not

--- functions/invalidate.js
+++ functions/invalidate.js
@@ -8,15 +8,22 @@ module.exports.invalidate = (event, context, callback) => {
     }
   };
   request(options, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
+    if (error) {
+      console.log('error:', error); // Print the error if one occurred
+      callback(error);
+      return;  
+    }
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
     callback(null, {
-      statusCode: 200,
+      statusCode: response.statusCode,
       headers: {
         "Access-Control-Allow-Origin": "*" // Required for CORS support to work
       },
-      body: JSON.stringify(event)
-    });  
+      body: JSON.stringify({
+        headers: response.headers,
+        body: 200 == response.statusCode ? JSON.parse(body) : body
+      })
+    });
   });
 };
